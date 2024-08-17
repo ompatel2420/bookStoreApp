3e5bfdc25fb55cc17b1e1db7cef259b29d140299
Ompatel2420 <ompatel201224@gmail.com>
2024-08-18 04:14:56 +0530

    commit

--- Frontend/src/components/Contact.jsx
+++ Frontend/src/components/Contact.jsx
@@ -1,6 +1,42 @@
 import React from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import axios from "axios";
+import toast from "react-hot-toast";
 
 function Contactus() {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const from = location.state?.from?.pathname || "/";
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = async (data) => {
+    const userInfo = {
+      fullname: data.fullname,
+      email: data.email,
+      password: data.password,
+    };
+    await axios
+      .post("http://localhost:4001/user/signup", userInfo)
+      .then((res) => {
+        console.log(res.data);
+        if (res.data) {
+          toast.success("Signup Successfully");
+          navigate(from, { replace: true });
+        }
+        localStorage.setItem("Users", JSON.stringify(res.data.user));
+      })
+      .catch((err) => {
+        if (err.response) {
+          console.log(err);
+          toast.error("Error: " + err.response.data.message);
+        }
+      });
+  };
 
   return (
     <>
@@ -15,6 +51,62 @@ function Contactus() {
             <span className="text-pink-500"> Us! :)</span>
           </h1>
         </div>
+        <form onSubmit={handleSubmit(onSubmit)} method="POST">
+              {/* if there is a button in form, it will close the modal */}
+              <div className="mt-4 space-y-2 flex justify-center"> 
+                <span>Name</span>
+                <br/>
+                <input
+                  type="text"
+                  placeholder="Enter your fullname"
+                  className="w-80 px-3 py-1 border rounded-md outline-none"
+                />
+                <br />
+                {errors.fullname && (
+                  <span className="text-sm text-red-500">
+                    This field is required
+                  </span>
+                )}
+              </div>
+              {/* Email */}
+              <div className="mt-4 space-y-2 flex justify-center">
+                <span>Email ID</span>
+                <br />
+                <input
+                  type="email"
+                  placeholder="Enter your email"
+                  className="w-80 px-3 py-1 border rounded-md outline-none"
+                />
+                <br />
+                {errors.email && (
+                  <span className="text-sm text-red-500">
+                    This field is required
+                  </span>
+                )}
+              </div>
+              {/* Password */}
+              <div className="mt-4 space-y-2 flex justify-center">
+                <label>Message</label>
+                <br />
+                <input
+                  type="text"
+                  placeholder="Hi there, Here is the Book name that i need"
+                  className="w-80 px-3 py-1 border rounded-md outline-none"
+                />
+                <br />
+                {errors.password && (
+                  <span className="text-sm text-red-500">
+                    This field is required
+                  </span>
+                )}
+              </div>
+              {/* Button */}
+              <div className="flex justify-around mt-4">
+                <button className="bg-pink-500 text-white rounded-md px-3 py-1 hover:bg-pink-700 duration-200">
+                  Submit
+                </button>
+              </div>
+            </form>
       </div>
     </>
   );
